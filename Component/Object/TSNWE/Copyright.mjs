57cb321ee0529e29a66c220928fa761c4aa6eda7
Ascellayn <ascellayn.sagittarii+github@sirio-network.com>
2026-06-02 12:03:36 +0200

    B¤Component/Copyright | The Sirio Network has technically been in operation since 2020, not 2022 when its SGN origins were discarded.

--- Component/Object/TSNWE/Copyright.mjs
+++ Component/Object/TSNWE/Copyright.mjs
@@ -12,7 +12,7 @@ export default class TSNJS_Copyright extends BASE_OBJECT {
 		this.Override_DOM = Common.ROOT;
 		this.Override_Pseudo = "After";
 
-		this.HTML = `<a href="/contact"><p>The Sirio Network © 2022-2026 | All rights reserved.${(Extra != "") ? `</p><p>${Extra}` : ""}</p></a>`;
+		this.HTML = `<a href="/contact"><p>The Sirio Network © 2020-2026 | All rights reserved.${(Extra != "") ? `</p><p>${Extra}` : ""}</p></a>`;
 	};
 };
 
